542d8b258d0d5b7b20918c6e3caa06979dcb78e8
Nicholaus Adhyatma <nicholausadhyatma@gmail.com>
2022-10-23 13:25:50 +0700

    fix-all2

--- src/assets/scroll.js
+++ src/assets/scroll.js
@@ -5,29 +5,26 @@
 // window.onscroll = scrollFunction;
 var pos = 0;
 
-
 $(window).bind("mousewheel touchmove", function (event) {
   if (event.originalEvent.wheelDelta >= 0) {
     if (0 < pos) {
-      pos -= 400;
+      pos -= $(window).width();
       $("#yscroll").scrollLeft(pos);
     }
   } else {
     if ($(window).width() > pos) {
-      pos += 400;
+      pos += $(window).width();
       $("#yscroll").scrollLeft(pos);
     }
   }
 
-  if ($("#yscroll").scrollLeft() >= $(window).width()) {
+  if ($("#yscroll").scrollLeft() >= $(window).width() ) {
     $("#header").show();
     $("#content").show();
     $('body').css('overflow', 'visible')
   } else {
+    $('body').css('overflow', 'hidden')
     $("#header").hide(500);
     $("#content").hide(500);
   }
-
 });
-
-
